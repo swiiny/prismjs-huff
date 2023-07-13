f927c7ce22bdae1b7c5f315daf2c8b0c7d48cb81
Jeremy Theintz <jeremy@appogee.ch>
2023-07-13 10:03:32 +0200

    full huff language support

--- src/index.js
+++ src/index.js
@@ -1,39 +1,67 @@
 // huff grammar
 function huff(Prism) {
-	Prism.languages.huff = {
+	Prism.languages.huff = Prism.languages.extend('clike', {
+		keyword:
+			/\b(?:define|macro|fn|takes|returns|jumpi|revert|pop|continue|caller|calldataload|shr|sload|eq|jump|jumpdest|mstore|return|dup1|add|mul|log3|codecopy|mload|lt|err|calldata|callvalue|add|ASSERT)\b/,
+		operator: /:=|=/,
+		builtin:
+			/\b(?:__ERROR|__EVENT_HASH|__FUNC_SIG|PanicError|Error|FREE_STORAGE_POINTER|__tablesize|__tablestart|__FUNC_SIG|__EVENT_HASH|__ERROR|__RIGHTPAD|__codesize)\b/,
+		function: /\b\w+(?=\(.*\))/,
+		number: /\b0x[\da-f]+\b|(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:e[+-]?\d+)?/i,
+		punctuation: /[{}[\];(),.:]/,
 		comment: [
 			{
-				pattern: /(^|[^\\:])\/\/.*/,
+				pattern: /(^|[^\\])\/\*[\s\S]*?(?:\*\/|$)/,
 				lookbehind: true,
 				greedy: true
 			},
 			{
-				pattern: /\/\*[\s\S]*?\*\//,
+				pattern: /(^|[^\\:])\/\/.*/,
+				lookbehind: true,
 				greedy: true
 			}
-		],
-		string: {
-			pattern: /(["'])(?:\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
-			greedy: true
-		},
-		keyword: {
+		]
+	});
+
+	Prism.languages.insertBefore('huff', 'keyword', {
+		directive: {
 			pattern:
-				/\b(?:define|function|event|macro|fn|takes|returns|constant|error|view|pure|payable|nonpayable|indexed|include)\b/,
-			greedy: true
+				/#(?:include|define function|define event|define constant|define macro| define test|define jumptable|define error|calldata|fn|table|test)\b/,
+			inside: {
+				punctuation: /#/
+			}
 		},
-		'built-in': {
-			pattern: /\b(?:__FUNC_SIG|__EVENT_HASH|__ERROR|mint|burn|stop|dup1|revert)\b/,
-			greedy: true
+		constant: {
+			pattern: /\[\w+\]/,
+			inside: {
+				punctuation: /\[|\]/
+			}
 		},
-		number: /\b(?:0x[\da-fA-F]+|\d+)\b/,
-		operator: /[-+*\/%!^]=?|=[=>]?|&[&=]?|\|[|=]?|<<?=?|>>?=?|[@?]/,
-		punctuation: /[{}[\];(),:]/,
-		'jump-label': {
-			pattern: /\bJUMPDEST\b/,
-			alias: 'function',
-			greedy: true
+		jumplabel: {
+			pattern: /\b\w+:/,
+			inside: {
+				punctuation: /:/
+			}
+		},
+		'macro-arg': {
+			pattern: /<\w+>/,
+			inside: {
+				punctuation: /<|>/
+			}
+		},
+		table: {
+			pattern: /\b\w+\s+\{[^}]+\}/,
+			inside: {
+				punctuation: /\{|\}/
+			}
+		},
+		test: {
+			pattern: /#\[[^\]]+\]/,
+			inside: {
+				punctuation: /#|\[|\]/
+			}
 		}
-	};
+	});
 }
 
 huff.displayName = 'huff';
